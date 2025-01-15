24ed06491b5e6e8edff83c609360f31cf3586293
flint eggebeen <eggebeenflint@gmail.com>
2025-01-15 09:31:17 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,21 +1,14 @@
 <?php
 session_start();
 
-/**
- * Voor de MAC gebruikers;
- */
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "root";
-$dbname = "youtube-test";
 
 /**
  * Voor de Windows gebruikers;
  */
-// $dbhost = "localhost";
-// $dbuser = "root";
-// $dbpass = "";
-// $dbname = "youtube-clone";
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $dbname = "youtube-clone";
 
 $con = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
